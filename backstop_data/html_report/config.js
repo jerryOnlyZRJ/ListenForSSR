932d6b21677b922f4b94d772aca43ca44ea18d4a
ranjayzheng <ranjayzheng@tencent.com>
2018-07-28 22:10:31 +0800

    add backstopjs

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,84 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_listenfor_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180728-220725/backstop_default_listenfor_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_listenfor_0_document_0_phone.png",
+        "label": "listenfor",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://listenfor.jerryonlyzrj.com/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "2.09",
+          "analysisTime": 42
+        },
+        "diffImage": "../bitmaps_test/20180728-220725/failed_diff_backstop_default_listenfor_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_listenfor_editor_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180728-220725/backstop_default_listenfor_editor_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_listenfor_editor_0_document_0_phone.png",
+        "label": "listenfor editor",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://listenfor.jerryonlyzrj.com/editor",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.69",
+          "analysisTime": 42
+        },
+        "diffImage": "../bitmaps_test/20180728-220725/failed_diff_backstop_default_listenfor_editor_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_listenfor_dashboard_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180728-220725/backstop_default_listenfor_dashboard_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_listenfor_dashboard_0_document_0_phone.png",
+        "label": "listenfor dashboard",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://listenfor.jerryonlyzrj.com/dashboard",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "96.82",
+          "analysisTime": 38
+        },
+        "diffImage": "../bitmaps_test/20180728-220725/failed_diff_backstop_default_listenfor_dashboard_0_document_0_phone.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
