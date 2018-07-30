547f7c051e946cc72db80b18a8020c57cab8aa75
ranjayzheng <ranjayzheng@tencent.com>
2018-07-30 16:44:20 +0800

    update backstop reference

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,8 +3,8 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_listenfor_login_0_document_0_phone.png",
-        "test": "../bitmaps_test/20180729-105120/backstop_default_listenfor_login_0_document_0_phone.png",
+        "reference": "..\\bitmaps_reference\\backstop_default_listenfor_login_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20180730-164008\\backstop_default_listenfor_login_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_listenfor_login_0_document_0_phone.png",
         "label": "listenfor_login",
@@ -20,17 +20,17 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "2.09",
-          "analysisTime": 36
+          "misMatchPercentage": "2.07",
+          "analysisTime": 35
         },
-        "diffImage": "../bitmaps_test/20180729-105120/failed_diff_backstop_default_listenfor_login_0_document_0_phone.png"
+        "diffImage": "..\\bitmaps_test\\20180730-164008\\failed_diff_backstop_default_listenfor_login_0_document_0_phone.png"
       },
       "status": "fail"
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_listenfor_editor_0_document_0_phone.png",
-        "test": "../bitmaps_test/20180729-105120/backstop_default_listenfor_editor_0_document_0_phone.png",
+        "reference": "..\\bitmaps_reference\\backstop_default_listenfor_editor_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20180730-164008\\backstop_default_listenfor_editor_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_listenfor_editor_0_document_0_phone.png",
         "label": "listenfor_editor",
@@ -46,17 +46,17 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.69",
-          "analysisTime": 36
+          "misMatchPercentage": "0.50",
+          "analysisTime": 34
         },
-        "diffImage": "../bitmaps_test/20180729-105120/failed_diff_backstop_default_listenfor_editor_0_document_0_phone.png"
+        "diffImage": "..\\bitmaps_test\\20180730-164008\\failed_diff_backstop_default_listenfor_editor_0_document_0_phone.png"
       },
       "status": "fail"
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_listenfor_dashboard_0_document_0_phone.png",
-        "test": "../bitmaps_test/20180729-105120/backstop_default_listenfor_dashboard_0_document_0_phone.png",
+        "reference": "..\\bitmaps_reference\\backstop_default_listenfor_dashboard_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20180730-164008\\backstop_default_listenfor_dashboard_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_listenfor_dashboard_0_document_0_phone.png",
         "label": "listenfor_dashboard",
@@ -72,10 +72,36 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "96.82",
+          "misMatchPercentage": "1.63",
+          "analysisTime": 42
+        },
+        "diffImage": "..\\bitmaps_test\\20180730-164008\\failed_diff_backstop_default_listenfor_dashboard_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_listenfor_register_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20180730-164008\\backstop_default_listenfor_register_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_listenfor_register_0_document_0_phone.png",
+        "label": "listenfor_register",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://listenfor.jerryonlyzrj.com/register",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "1.95",
           "analysisTime": 37
         },
-        "diffImage": "../bitmaps_test/20180729-105120/failed_diff_backstop_default_listenfor_dashboard_0_document_0_phone.png"
+        "diffImage": "..\\bitmaps_test\\20180730-164008\\failed_diff_backstop_default_listenfor_register_0_document_0_phone.png"
       },
       "status": "fail"
     }
