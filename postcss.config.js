0ac6e084a95883833d00b1f7f83f5955ecb0eec0
ranjayzheng <ranjayzheng@tencent.com>
2018-09-29 20:41:49 +0800

    add autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   plugins: {
+    'autoprefixer': {
+      "browsers": [
+        "iOS >= 8",
+        "Firefox >= 20",
+        "Android > 4.4"
+      ]
+    },
     'postcss-preset-env': {
       stage: 0
     },
